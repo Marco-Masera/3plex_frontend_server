a8f0c13ab22e776df65a5ee2918cb3d0d36c1010
Marco-Masera <mirco.masera@gmail.com>
2023-05-11 12:44:03 +0200

    Dev: added results_mng module

---
+++ server/triplex_frontend/results_mng/services.py
@@ -0,0 +1,11 @@
+
+class ResultsMngServices:
+    def get_by_token():
+        pass 
+
+    def delete_data_by_token(token: int):
+        pass 
+
+    def initialize_data_section(token: int, ssRNA_fasta, ssRNA_id = None):
+        #Initialize data section, keep track of sequence and id, used later for computing the conservation
+        pass
