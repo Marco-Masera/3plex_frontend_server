dae8071b8530d340b9f36dcd33c84fc200ccf09b
Marco-Masera <mirco.masera@gmail.com>
2023-05-11 18:48:42 +0200

    Dev

--- server/triplex_frontend/triplex_frontend/urls.py
+++ server/triplex_frontend/triplex_frontend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from triplex import urls as server_api
+from results_mng import urls as results_mng_api
 
 urlpatterns = [
     path('api/', include(server_api)),
+    path('results/', include(results_mng_api))
 ]
